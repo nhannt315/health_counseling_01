890498f849076c4617c4362914b8bd5865062269
Nguyễn Thành Nhân <nhannt.bk@gmail.com>
2018-08-10 08:12:16 +0700

    switch to devise

--- app/controllers/searchs_controller.rb
+++ app/controllers/searchs_controller.rb
@@ -1,21 +1,28 @@
 class SearchsController < ApplicationController
+  before_action :fetch_questions, only: [:index]
+
   def index
-    if params[:query]
-      @questions = Question.search(params[:query])
-                           .limit(Settings.home.suggest_limit)
-    end
     respond_to do |format|
       format.html
       format.json do
-        render json: @questions.map{|question|
+        render json: @questions.map do |question|
           {
             id: question.id,
             suggest: question.content.truncate(Settings.home.suggest_text_len),
             link: question_url(question), content: question.content
-          }}
+          }
+        end
       end
     end
   end
 
   def show; end
+
+  private
+
+  def fetch_questions
+    return unless params[:query]
+    @questions = Question.search(params[:query])
+                         .limit(Settings.home.suggest_limit)
+  end
 end
