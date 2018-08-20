c24c3aa4f507cf32df468441fbf5de93fbeeb915
Nguyễn Thành Nhân <nhannt.bk@gmail.com>
2018-08-20 09:18:38 +0700

    Fix bug

--- app/assets/javascripts/channels/notifications.js
+++ app/assets/javascripts/channels/notifications.js
@@ -1,10 +1,8 @@
 App.notifications = App.cable.subscriptions.create("NotificationsChannel", {
   connected: function () {
   },
-
   disconnected: function () {
   },
-
   received: function (data) {
     $count = $('#notifications-count');
     let count = parseInt($count.text()) + 1;
